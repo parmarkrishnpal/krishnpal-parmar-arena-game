b0875a37568fa6970e5be3f40ae819d007da1659
cw-krishnpal-parmar <krishnpal@carwale.com>
2024-06-30 22:01:11 +0530

    Adding comments in Player and Arena Class

--- src/modules/Arena.js
+++ src/modules/Arena.js
@@ -1,116 +1,159 @@
-// @ts-nocheck
 import { rollDice } from "../utils/common.js";
-import Player from "./Player.js";
+import { Player } from "./Players.js";
 
-export default class Arena {
+export class Arena {
+  /**
+   * @class
+   * This class provides an interface to manage an arena where players can be added, deleted, displayed, and made to battle each other.
+   * It keeps track of the total number of players and their respective details using a Map.
+   * Example usage:
+   *   const arena = new Arena();
+   *   arena.addPlayer('John', 100, 50, 30);
+   *   arena.displayPlayers();
+   *   arena.battle(0, 1);
+   *
+   * @property {number} totalPlayers - Total players present in the arena.
+   * @property {Map<number, Player>} Players - A Map data structure to keep details of players.
+   */
+
   constructor() {
-    this.total_players = 0;
+    this.totalPlayers = 0;
     this.Players = new Map();
     console.log("Welcome to the arena!!!\n");
   }
 
+  /**
+   * Checks if a player with the given id exists.
+   * @param {number} id - The player's id.
+   * @returns {boolean} - True if player exists, otherwise false.
+   */
   isPresent(id) {
     return this.Players.has(id);
   }
 
+  /**
+   * Gets the current number of players in the arena.
+   * @returns {number} - The count of players.
+   */
   getPlayerCount() {
     return this.Players.size;
   }
 
+  /**
+   * Adds a new player to the arena.
+   * @param {string} name - The player's name.
+   * @param {number} health - The player's health.
+   * @param {number} strength - The player's strength.
+   * @param {number} attack - The player's attack power.
+   * @returns {number} - The new player's id.
+   */
   addPlayer(name, health, strength, attack) {
-    if (health <= 0) {
-      console.log("Health should be a positive integer.");
-      return -1;
-    }
-    if (strength <= 0) {
-      console.log("Strength should be a positive integer.");
-      return -1;
-    }
-    if (attack <= 0) {
-      console.log("Attack should be a positive integer.");
+    if (health <= 0 || strength <= 0 || attack <= 0) {
+      console.error(
+        "Health, Strength, and Attack should be positive integers."
+      );
       return -1;
     }
 
-    const id = this.total_players;
+    const id = this.totalPlayers;
     const newPlayer = new Player(id, name, health, strength, attack);
     this.Players.set(id, newPlayer);
-    this.total_players += 1;
+    this.totalPlayers += 1;
 
     return id;
   }
 
+  /**
+   * Deletes a player from the arena by id.
+   * @param {number} id - The player's id.
+   */
   deletePlayer(id) {
     if (this.Players.has(id)) {
       const player = this.Players.get(id);
-      console.log(`${player?.name} has been un-alived...\n`);
+      console.log(`${player.name} has been un-alived...\n`);
       this.Players.delete(id);
     } else {
-      console.log(`No player with id = ${id} exists.\n`);
+      console.error(`No player with id = ${id} exists.\n`);
     }
   }
 
+  /**
+   * Displays all players in the arena.
+   */
   displayPlayers() {
-    console.log("|\tId\t|\tName\t|\tHealth\t|\tStrength\t|\tAttack\t|");
+    const playerArray = [];
     for (const [id, player] of this.Players) {
-      const { name, health, strength, attack } = player;
-      console.log(
-        `|\t${id}\t|\t${name}\t|\t${health}\t|\t${strength}\t|\t${attack}\t|`
-      );
+      playerArray.push({
+        Id: id,
+        Name: player.name,
+        Health: player.health,
+        Strength: player.strength,
+        Attack: player.attack,
+      });
     }
-    console.log("\n");
+    console.clear();
+    console.table(playerArray, ["Id", "Name", "Health", "Strength", "Attack"]);
   }
 
-  battle(id_first, id_second) {
-    if (id_first === id_second) {
-      console.log("Id's cannot be the same for both the players.\n");
+  /**
+   * Initiates a battle between two players.
+   * @param {number} firstPlayerID - The id of the first player.
+   * @param {number} secondPlayerID - The id of the second player.
+   * @returns {Object} - The result of the battle.
+   */
+  battle(firstPlayerID, secondPlayerID) {
+    if (firstPlayerID === secondPlayerID) {
+      console.error("Ids cannot be the same for both the Players.\n");
       return {};
-    } else if (!this.Players.has(id_first)) {
-      console.log(`No player with id = ${id_first} exists.\n`);
+    }
+    if (!this.isPresent(firstPlayerID)) {
+      console.error(`No player with id = ${firstPlayerID} exists.\n`);
       return {};
-    } else if (!this.Players.has(id_second)) {
-      console.log(`No player with id = ${id_second} exists.\n`);
+    }
+    if (!this.isPresent(secondPlayerID)) {
+      console.error(`No player with id = ${secondPlayerID} exists.\n`);
       return {};
-    } else {
-      // extract the players from their ids
-      let attacker = this.Players.get(id_first);
-      let defender = this.Players.get(id_second);
-      console.log(
-        `\n____________${attacker.name} v/S ${defender.name}____________\n`
-      );
+    }
 
-      if (defender.health < attacker.health) {
-        // Player with the lower health attacks first
+    let attacker = this.Players.get(firstPlayerID);
+    let defender = this.Players.get(secondPlayerID);
+
+    if (defender.health < attacker.health) {
+      [attacker, defender] = [defender, attacker];
+    }
+
+    console.log(
+      `\n____________${attacker.name} v/s ${defender.name}____________\n`
+    );
+
+    const battleRound = (attacker, defender) => {
+      const attackingPower = attacker.attack * rollDice();
+      const defendingPower = defender.strength * rollDice();
+
+      console.log(
+        `${attacker.name} hits ${defender.name} with power: ${attackingPower}\n`
+      );
+      console.log(`${defender.name} defends with power: ${defendingPower}\n`);
+
+      if (attackingPower > defendingPower) {
+        defender.health -= attackingPower - defendingPower;
+        defender.health = Math.max(0, defender.health);
+      }
+
+      console.log(`${defender.name}'s health: ${defender.health}\n`);
+    };
+
+    while (defender.health > 0) {
+      battleRound(attacker, defender);
+      if (defender.health > 0) {
         [attacker, defender] = [defender, attacker];
       }
-
-      while (defender.health > 0) {
-        const attacking_power = attacker.attack * rollDice();
-        const defending_power = defender.strength * rollDice();
-
-        console.log(
-          `${attacker?.name} hits ${defender?.name} with power = ${attacking_power}\n`
-        );
-        console.log(
-          `${defender?.name} defends with power = ${defending_power}\n`
-        );
-
-        if (attacking_power > defending_power) {
-          defender.health -= attacking_power - defending_power;
-          defender.health = Math.max(0, defender.health);
-        }
-
-        console.log(`${defender?.name}'s health: ${defender.health}\n`);
-
-        if (defender.health > 0) {
-          [attacker, defender] = [defender, attacker];
-        }
-      }
-
-      const res = { winner: attacker.id, loser: defender.id };
-      console.log(`${attacker?.name} has emerged victorious!!!\n`);
-      this.deletePlayer(defender.id);
-
-      return res;
     }
+
+    const result = { winner: attacker.id, loser: defender.id };
+    console.log(`${attacker.name} has emerged victorious!!!\n`);
+    this.deletePlayer(defender.id);
+
+    return result;
   }
 }
--- src/modules/Player.js
+++ src/modules/Player.js
@@ -1,4 +1,19 @@
-export default class Player {
+export class Player {
+  /**
+   * @class
+   * This class represents a player in the game.
+   *
+   * It provides an interface to create player objects with specific attributes such as id, name, health, strength, and attack.
+   * Example usage:
+   *   const player = new Player(1, 'John', 100, 50, 30);
+   *
+   * @property {number} id - The unique identifier for the player.
+   * @property {string} name - The name of the player.
+   * @property {number} health - The health points of the player.
+   * @property {number} strength - The strength attribute of the player.
+   * @property {number} attack - The attack power of the player.
+   */
+
   constructor(id, name, health, strength, attack) {
     this.id = id;
     this.name = name;
